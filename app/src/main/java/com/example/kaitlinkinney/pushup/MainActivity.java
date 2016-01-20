baa1533ab76adec66fa47742aff5981240147490
KaitlinKinney <magykjack@cs.com>
2016-01-20 17:34:03 -0500

    Modified MainActivity Addd New File testAddFile.java

--- app/src/main/java/com/example/kaitlinkinney/pushup/MainActivity.java
+++ app/src/main/java/com/example/kaitlinkinney/pushup/MainActivity.java
@@ -41,5 +41,9 @@ public class MainActivity extends ActionBarActivity {
 
         int i= 1+1;
 
+        int ii = 2+2;
+
+        int iii= i + ii;
+
     }
 }
---
+++ app/src/main/java/com/example/kaitlinkinney/pushup/testAddFile.java
@@ -0,0 +1,7 @@
+package com.example.kaitlinkinney.pushup;
+
+/**
+ * Created by KaitlinKinney on 1/20/16.
+ */
+public class testAddFile {
+}
